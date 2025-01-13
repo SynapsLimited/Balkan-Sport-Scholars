904626fe34885178f67165597cb01e7fe59dd37a
SynapsLimited <synapslimited@gmail.com>
2025-01-13 12:52:42 +0100

    Analytics Update

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,9 @@ import { HelmetProvider } from 'react-helmet-async';
 import { Analytics } from "@vercel/analytics/react"
 
 
-
-
-
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import LoadingScreen from './components/LoadingScreen';
-
 
 
 import Home from './pages/Home';
